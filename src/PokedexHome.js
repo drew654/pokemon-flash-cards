984723e6cbc161356bfb5ba7048043ff9a1723b7
drew654 <drewpusey654@gmail.com>
2023-11-22 17:07:00 -0600

    Better lazy loading

--- src/PokedexHome.js
+++ src/PokedexHome.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import PokemonCard from './PokemonCard';
-import { Link } from 'react-router-dom';
-import { parseName } from './PokedexFunctions';
 const Pokedex = require('pokeapi-js-wrapper');
 const P = new Pokedex.Pokedex();
 
 const PokedexHome = () => {
-  const [pokemons, setPokemons] = useState([]);
   const pageSize = 20;
+  const pokedexSize = 1017;
+  const [pokemons, setPokemons] = useState(Array(pokedexSize).fill(null));
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
@@ -19,18 +18,9 @@ const PokedexHome = () => {
         const types = pokemonInfo.types.map(type => type.type.name);
         return { name: pokemon.name, number, types };
       }));
-
-      setPokemons(prev => {
-        const newPokemons = [...prev, ...pokemonData];
-        const uniquePokemons = Array.from(new Set(newPokemons.map(pokemon => pokemon.number))).map(number => {
-          return newPokemons.find(pokemon => pokemon.number === number);
-        });
-        uniquePokemons.sort((a, b) => a.number - b.number);
-        return uniquePokemons;
-      }); 
-
-      if (offset < 1017) {
-        setOffset(prev => Math.min(prev + pageSize, 1017 - pageSize));
+      setPokemons(prev => prev.map((item, index) => pokemonData.find(pokemon => pokemon.number - 1 === index) || item));
+      if (offset < pokedexSize) {
+        setOffset(offset + pageSize);
       }
     };
   
@@ -41,7 +31,7 @@ const PokedexHome = () => {
     <div>
       <h1>Pokedex</h1>
       <ul>
-        {pokemons.map(pokemon => <PokemonCard key={pokemon.number} pokemon={pokemon} />)}
+        {pokemons.map((pokemon, index) => pokemon ? <PokemonCard key={index} pokemon={pokemon} /> : <PokemonCard key={index} pokemon={{number: index + 1}} />)}
       </ul>
     </div>
   );
--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -3,22 +3,34 @@ import { parseName } from './PokedexFunctions';
 
 const PokemonCard = ({ pokemon }) => {
   return (
-    <Link to={`/pokedex/${pokemon.number}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+    pokemon.name ? (
+      <Link to={`/pokedex/${pokemon.number}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+        <div style={{ display: 'flex', borderStyle: 'solid', width: '500px', height: '150px' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', flex: 1, padding: '10px' }}>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <h2 style={{ width: '80px' }}>#{pokemon.number}</h2>
+              <h2>{parseName(pokemon.name)}</h2>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              {pokemon.types.map(type => (
+                <h3 key={type} style={{ marginRight: '10px' }}>{parseName(type)}</h3>
+              ))}
+            </div>
+          </div>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.number}.png`} alt={pokemon.name} style={{ height: '100%' }} />
+        </div>
+      </Link>
+    ) : (
+      <Link to={`/pokedex/${pokemon.number}`} style={{ textDecoration: 'none', color: 'inherit' }}>
       <div style={{ display: 'flex', borderStyle: 'solid', width: '500px', height: '150px' }}>
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', flex: 1, padding: '10px' }}>
           <div style={{ display: 'flex', flexDirection: 'row' }}>
             <h2 style={{ width: '80px' }}>#{pokemon.number}</h2>
-            <h2>{parseName(pokemon.name)}</h2>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-            {pokemon.types.map(type => (
-              <h3 key={type} style={{ marginRight: '10px' }}>{parseName(type)}</h3>
-            ))}
           </div>
         </div>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.number}.png`} alt={pokemon.name} style={{ height: '100%' }} />
       </div>
     </Link>
+    )
   );
 }
 
