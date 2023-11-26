ec52bae75e9f18f7e0196688b373b853281c19fb
drew654 <drewpusey654@gmail.com>
2023-11-26 15:56:24 -0600

    Started filters

---
+++ src/PokeAPIAdapter.js
@@ -0,0 +1,116 @@
+export const regions = [
+  {
+    name: "Kanto",
+    pokedexes: [
+      {
+        name: "kanto",
+        url: "https://pokeapi.co/api/v2/pokedex/2/"
+      }
+    ]
+  },
+  {
+    name: "Johto",
+    pokedexes: [
+      {
+        name: "original-johto",
+        url: "https://pokeapi.co/api/v2/pokedex/3/"
+      }
+    ]
+  },
+  {
+    name: "Hoenn",
+    pokedexes: [
+      {
+        name: "hoenn",
+        url: "https://pokeapi.co/api/v2/pokedex/4/"
+      }
+    ]
+  },
+  {
+    name: "Sinnoh",
+    pokedexes: [
+      {
+        name: "original-sinnoh",
+        url: "https://pokeapi.co/api/v2/pokedex/5/"
+      }
+    ]
+  },
+  {
+    name: "Unova",
+    pokedexes: [
+      {
+        name: "original-unova",
+        url: "https://pokeapi.co/api/v2/pokedex/8/"
+      }
+    ]
+  },
+  {
+    name: "Kalos",
+    pokedexes: [
+      {
+        name: "kalos-central",
+        url: "https://pokeapi.co/api/v2/pokedex/12/"
+      },
+      {
+        name: "kalos-coastal",
+        url: "https://pokeapi.co/api/v2/pokedex/13/"
+      },
+      {
+        name: "kalos-mountain",
+        url: "https://pokeapi.co/api/v2/pokedex/14/"
+      }
+    ]
+  },
+  {
+    name: "Alola",
+    pokedexes: [
+      {
+        name: "updated-alola",
+        url: "https://pokeapi.co/api/v2/pokedex/21/"
+      }
+    ]
+  },
+  {
+    name: "Galar",
+    pokedexes: [
+      {
+        name: "galar",
+        url: "https://pokeapi.co/api/v2/pokedex/27/"
+      },
+      {
+        name: "isle-of-armor",
+        url: "https://pokeapi.co/api/v2/pokedex/28/"
+      },
+      {
+        name: "crown-tundra",
+        url: "https://pokeapi.co/api/v2/pokedex/29/"
+      }
+    ]
+  },
+  {
+    name: "Hisui",
+    pokedexes: [
+      {
+        name: "hisui",
+        url: "https://pokeapi.co/api/v2/pokedex/30/"
+      }
+    ]
+  },
+  {
+    name: "Paldea",
+    pokedexes: [
+      {
+        name: "paldea",
+        url: "https://pokeapi.co/api/v2/pokedex/31/"
+      },
+      {
+        name: "kitakami",
+        url: "https://pokeapi.co/api/v2/pokedex/32/"
+      },
+      {
+        name: "indigo-disk",
+        url: "https://pokeapi.co/api/v2/pokedex/33/"
+      }
+    ]
+  }
+]
--- src/PokemonFlashCards.js
+++ src/PokemonFlashCards.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { parseName } from './PokemonFunctions';
+import { regions } from './PokeAPIAdapter';
 const Pokedex = require('pokeapi-js-wrapper');
 const P = new Pokedex.Pokedex();
 
@@ -10,6 +11,8 @@ const PokemonFlashCards = () => {
   const [currentPokemon, setCurrentPokemon] = useState(null);
   const [types, setTypes] = useState([]);
   const [typeSelection, setTypeSelection] = useState([]);
+  const [filters, showFilters] = useState(false);
+  const [selectedRegions, setSelectedRegions] = useState([]);
 
   const TypeButton = (type) => (
     <div>
@@ -62,13 +65,28 @@ const PokemonFlashCards = () => {
 
   useEffect(() => {
     const fetchPokemons = async () => {
-      const response = await P.getPokemonsList();
-      const pokemonNames = response.results.map((pokemon) => pokemon.name);
-      setPokemons(pokemonNames);
+      const matchingRegions = regions.filter((region) => selectedRegions.map((r) => r.name).includes(region.name));
+      if (selectedRegions.length > 0) {
+        const pokemonNames = [];
+        P.resource(
+          matchingRegions.flatMap((region) => region.pokedexes.map((pokedex) => pokedex.url))
+        ).then( response => {
+          response.forEach((region) => {
+            const regionPokemonNames = region.pokemon_entries.map((pokemon) => pokemon.pokemon_species.name);
+            pokemonNames.push(...regionPokemonNames.filter(name => !pokemonNames.includes(name)));
+          });
+        });
+        setPokemons(pokemonNames);
+      }
+      else {
+        const response = await P.getPokemonsList();
+        const pokemonNames = response.results.map((pokemon) => pokemon.name);
+        setPokemons(pokemonNames);
+      }
     };
 
     fetchPokemons();
-  }, []);
+  }, [selectedRegions]);
 
   useEffect(() => {
     const fetchTypes = async () => {
@@ -95,22 +113,74 @@ const PokemonFlashCards = () => {
       height: '100vh',
     }}>
       {gameState === 'waiting to start' && (
-        <div
-          onClick={() => {
-            setGameState('showing pokemon');
-            getRandomPokemon().then((pokemon) => setCurrentPokemon(pokemon));
-          }}
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '4em',
-            width: '12em',
-            fontSize: '2rem',
-            cursor: 'pointer',
-            border: '1px solid black',
-          }}
-        >Start</div>
+        <div>
+          <div
+            style={{
+              position: 'fixed', 
+              top: 0,
+            }}
+          >
+            <div
+              onClick={() => showFilters(!filters)}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '1.2em',
+                width: '3em',
+                fontSize: '2rem',
+                cursor: 'pointer',
+                border: '1px solid black',
+                userSelect: 'none',
+                backgroundColor: filters ? 'lightgray' : 'white',
+              }}
+            >Filters</div>
+            {filters && (
+              <div>
+                <div style={{ fontSize: '1.5em' }}>
+                  Regions
+                </div>
+                {regions.map((region) => (
+                  <div key={region}>
+                    <input
+                      type="checkbox"
+                      checked={selectedRegions.includes(region)}
+                      onChange={(e) => {
+                        const updatedRegions = [...selectedRegions];
+                        if (e.target.checked) {
+                          updatedRegions.push(region);
+                        } else {
+                          const index = updatedRegions.indexOf(region);
+                          updatedRegions.splice(index, 1);
+                        }
+                        setSelectedRegions(updatedRegions);
+                      }}
+                    />
+                    <label>{parseName(region.name)}</label>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          <div
+            onClick={() => {
+              setGameState('showing pokemon');
+              getRandomPokemon().then((pokemon) => setCurrentPokemon(pokemon));
+            }}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '4em',
+              width: '12em',
+              fontSize: '2rem',
+              cursor: 'pointer',
+              border: '1px solid black',
+              marginTop: '1em',
+              userSelect: 'none',
+            }}
+          >Start</div>
+        </div>
       )}
       {(gameState === 'showing pokemon' || gameState === 'all types selected') && (
         <div
