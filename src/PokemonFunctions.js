11a9ac054bbc54c51734237bf2090d158eb62b9c
Drew Pusey <drewpusey654@gmail.com>
2023-12-03 19:03:06 -0600

    Restarting name parsing

--- src/PokemonFunctions.js
+++ src/PokemonFunctions.js
@@ -1,13 +1,39 @@
 export const parseName = name => {
-  const words = name?.split('-');
-  if (words) {
-    if (words[words.length - 1] === 'mega') {
-      words.unshift(words.pop());
+  const parsedName = {};
+
+  parsedName.print = () => {
+    let printName = '';
+
+    // Mega
+    if (parsedName.mega) {
+      printName += 'Mega ';
     }
-    else if (words[words.length - 1] === 'gmax') {
-      words.pop();
-      words.unshift('Gigantamax');
+
+    // Rest of name
+    printName += name.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+
+    // Mega X/Y
+    if (parsedName?.mega?.length === 6) {
+      printName += ' ' + parsedName.mega.charAt(5);
+    }
+    return printName;
+  }
+
+  // Mega (X/Y)
+  if (name.includes('mega')) {
+    if (name.includes('mega-x')) {
+      parsedName.mega = 'Mega X';
+      name = name.replace('-mega-x', '');
+    }
+    else if (name.includes('mega-y')) {
+      parsedName.mega = 'Mega Y';
+      name = name.replace('-mega-y', '');
+    }
+    else {
+      parsedName.mega = 'Mega';
+      name = name.replace('-mega', '');
     }
   }
-  return words?.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  
+  return parsedName.print();
 }
